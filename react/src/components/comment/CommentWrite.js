f855cbb7351e669a103ca9eda7b72f8352a6db44
ParkJiSung <gggg0195@gmail.com>
2023-11-16 15:40:26 +0900

    update post /commentrestAPI 명세서(주석처리)

--- react/src/components/comment/CommentWrite.js
+++ react/src/components/comment/CommentWrite.js
@@ -1,4 +1,52 @@
+/*
+API 명세서: 댓글 생성
 
+URL: /comment
+
+Method: POST
+
+URL Params:
+  Required: BoardSeq=[integer] - 게시글의 시퀀스 번호
+
+Data Params:
+  id=[string] - 사용자 ID
+  content=[string] - 댓글 내용
+  BoardSeq=[integer] - 게시글의 시퀀스 번호 (Body에도 포함될 수 있음)
+
+Headers:
+  Authorization=[string] - 인증 토큰 (예: Bearer token)
+
+Success Response:
+  Code: 200 OK
+  Content:{
+    "seq": [integer], // 댓글의 시퀀스 번호
+    "message": "댓글을 성공적으로 등록했습니다 :D"
+  }
+Error Response:
+  Code: 400 BAD REQUEST
+  Content:{
+    "error": "Invalid input"
+  }
+OR
+  Code: 500 INTERNAL SERVER ERROR
+  Content:{
+    "error": "Server error"
+  }
+예시 Call:
+axios.post('http://localhost:8080/comment', {
+  id: 'user123',
+  content: '이것은 댓글입니다.',
+  BoardSeq: 42
+}, {
+  params: { BoardSeq: 42 },
+  headers: { Authorization: 'Bearer token' }
+});
+  
+Notes:
+댓글 생성 시 사용자 인증이 필요할 수 있음.
+BoardSeq는 URL 파라미터로도 전송되지만, 요청 본문에도 포함될 수 있음.
+서버는 성공적으로 댓글을 생성한 후, 생성된 댓글의 seq를 포함한 객체를 응답으로 반환해야 함.
+*/
 
 import axios from "axios";
 import { useContext, useState } from "react";
