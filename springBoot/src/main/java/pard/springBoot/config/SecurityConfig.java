93885f19e45c1480d8650a7e481b0c6bf86f3b6a
ParkJiSung <gggg0195@gmail.com>
2023-11-17 20:42:53 +0900

    update config

--- springBoot/src/main/java/pard/springBoot/config/SecurityConfig.java
+++ springBoot/src/main/java/pard/springBoot/config/SecurityConfig.java
@@ -1,4 +1,47 @@
 package pard.springBoot.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
 public class SecurityConfig {
+
+    /**
+     * 이 메서드는 정적 자원(HTML, CSS, JavaScript같은 것들)에 대해 보안을 적용하지 않도록 설정.
+     * 이들에 대해 보안을 적용하지 않음으로써 성능을 향상ㄱㄱ.
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomAuthenticationFilter customAuthenticationFilter,
+            JwtAuthorizationFilter jwtAuthorizationFilter
+    ) throws Exception {
+        log.debug("[+] WebSecurityConfig Start !!! ");
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/resources/**").permitAll()
+                        .requestMatchers("/main/rootPage").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthorizationFilter, BasicAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .successHandler(new SimpleUrlAuthenticationSuccessHandler("/main/rootPage"))
+                        .permitAll()
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
 }
--- springBoot/src/main/java/pard/springBoot/controller/UserController.java
+++ springBoot/src/main/java/pard/springBoot/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pard.springBoot.dto.request.SignUpRequest;
 import pard.springBoot.dto.response.SignUpResponse;
 import pard.springBoot.entity.User;
 
@@ -14,8 +15,8 @@ import pard.springBoot.entity.User;
 @RequestMapping("/api/user")
 public class UserController {
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signUp(@RequestBody JoinRequest joinRequest) {
-        User user = userService.signUp(joinRequest);
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest signUpRequest) {
+        User user = userService.signUp(signUpRequest);
         // 회원가입 처리 후 적절한 HTTP 응답 반환
     }
 }
